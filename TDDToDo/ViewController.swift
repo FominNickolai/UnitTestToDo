98815b8144e64f5e68272783b0274115b2b79758
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-17 11:20:29 +0300

    Initial Commit

---
+++ TDDToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDDToDo
+//
+//  Created by Fomin Mykola on 8/17/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
